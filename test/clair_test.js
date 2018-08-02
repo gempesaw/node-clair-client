d5cecde98b1135219edb167fea9b2fa4ce81289f
Daniel Gempesaw <gempesaw@gmail.com>
2018-08-02 14:09:47 -0400

    Close tests manually when done
    just for fun as an alternative to mocha --exit. There's ... probably a
    better way to do this.

--- test/clair_test.js
+++ test/clair_test.js
@@ -3,6 +3,28 @@ const express = require('express');
 const Clair = require('..');
 
 describe('Clair', () => {
+  let server; // fakeClair() writes the server to this global variable
+              // so we can close it afterwards. awk!
+
+  const fakeClair = ({
+    analysis = { Layer: { Features: [] } },
+    upload = (req, res, next) => { res.statusCode = 201; res.json({ done: 'done' }); }
+  } = {}) => new Promise((resolve, reject) => {
+    const app = express();
+    app.post('*', upload);
+
+    app.get('/v1/layers/name', (req, res, next) => res.json(analysis));
+
+    server = app.listen(0, (err) => {
+      if (err) {
+        reject(err);
+      }
+
+      const { port } = server.address();
+      resolve(`http://0.0.0.0:${port}`);
+    });
+  });
+
   it('should analyze images without vulnerabilities', async () => {
     const clairAddress = await fakeClair();
     const clair = new Clair({ clairAddress });
@@ -13,6 +35,20 @@ describe('Clair', () => {
     });
 
     expect(isVulnerable).to.equal(false);
+  });
+
+  it('should analyze images and return the full response', async () => {
+    const clairAddress = await fakeClair();
+    const clair = new Clair({ clairAddress });
+
+    const ret = await clair.analyze({
+      image: 'image',
+      registry: fakeRegistry,
+      resolveWithFullResponse: true
+    });
+
+
+    expect(ret).to.eql({ Layer: { Features: [] } });
   });
 
   it('should analyze images with vulnerabilities', async () => {
@@ -56,27 +92,14 @@ describe('Clair', () => {
       expect(err.message).to.match(/upload.*fail/);
     }
   });
+
+  afterEach(() => {
+    if (server) {
+      server.close();
+    }
+  });
 });
 
 const fakeRegistry = {
   getLayers: () => ([{  Name: 'first-name', Path: 'first-path' }, {  Name: 'name', Path: 'path' }])
 };
-
-const fakeClair = ({
-  analysis = { Layer: { Features: [] } },
-  upload = (req, res, next) => { res.statusCode = 201; res.json({ done: 'done' }); }
-} = {}) => new Promise((resolve, reject) => {
-  const app = express();
-  app.post('*', upload);
-
-  app.get('/v1/layers/name', (req, res, next) => res.json(analysis));
-
-  const server = app.listen(0, (err) => {
-    if (err) {
-      reject(err);
-    }
-
-    const { port } = server.address();
-    resolve(`http://0.0.0.0:${port}`);
-  });
-});
