ece52ba4ad1cf5796e85bc48f2e5849322d4494a
Daniel Gempesaw <gempesaw@gmail.com>
2018-08-17 16:47:25 -0400

    Add support for v1 docker registry API responses

--- test/docker-registry_test.js
+++ test/docker-registry_test.js
@@ -35,18 +35,57 @@ describe('docker registry', () => {
   it('should get manifests for a public registry image', async () => {
     registry = new DockerRegistry('image:tag');
     realGetManifest = registry.registry.getManifest;
-    registry.registry.getManifest = (opts, cb) => cb(null, { config: { digest: 'digest' }, layers: [{ digest: 'some-blob' }] });
+    const manifest = {
+      schemaVersion: 2,
+      config: { digest: 'sha256:digest' },
+      layers: [{
+        digest: 'sha256:some-blob1'
+      }, {
+        digest: 'sha256:some-blob2'
+      }]
+    };
+    registry.registry.getManifest = (opts, cb) => cb(null, manifest);
     const layers = await registry.getLayers();
     expect(layers).to.eql([{
-      Name: 'digestsome-blob',
-      Path: 'https://registry.hub.docker.com/v2/library/image/blobs/some-blob'
+      Name: 'digestsome-blob1',
+      Path: 'https://registry.hub.docker.com/v2/library/image/blobs/sha256:some-blob1'
+    }, {
+      Name: 'digestsome-blob2',
+      Path: 'https://registry.hub.docker.com/v2/library/image/blobs/sha256:some-blob2'
+    }]);
+  });
+
+  it('should get manifests via a v1 schema', async () => {
+    registry = new DockerRegistry('image:tag');
+    realGetManifest = registry.registry.getManifest;
+    const manifest = {
+      schemaVersion: 1,
+      fsLayers: [
+        { blobSum: 'sha256:some-blob2' },
+        { blobSum: 'sha256:some-blob1' }
+      ]
+    };
+    registry.registry.getManifest = (opts, cb) => cb(null, manifest);
+    const layers = await registry.getLayers(1);
+    expect(layers).to.eql([{
+      Name: 'some-blob1',
+      Path: 'https://registry.hub.docker.com/v2/library/image/blobs/sha256:some-blob1'
+    }, {
+      Name: 'some-blob2',
+      Path: 'https://registry.hub.docker.com/v2/library/image/blobs/sha256:some-blob2'
     }]);
   });
 
   it('should get manifests for a private registry image', async () => {
     registry = new DockerRegistry('some.private.registry/image:tag');
     realGetManifest = registry.registry.getManifest;
-    registry.registry.getManifest = (opts, cb) => cb(null, { config: { digest: 'digest' }, layers: [{ digest: 'some-blob' }] });
+    const manifest = {
+      schemaVersion: 2,
+      config: { digest: 'digest' },
+      layers: [{ digest: 'some-blob' }]
+    };
+    registry.registry.getManifest = (opts, cb) => cb(null, manifest);
+
     const layers = await registry.getLayers();
     expect(layers).to.eql([{
       Name: 'digestsome-blob',
@@ -67,14 +106,29 @@ describe('docker registry', () => {
     }
   });
 
-  // generally skipped to avoid a network call
-  it.skip('should actually successfully look up an actual docker image', async () => {
-    registry = new DockerRegistry('alpine');
-    const layers = await registry.getLayers();
-    expect(layers).to.eql([{
-      Name: '11cd0b38bc3ceb958ffb2f9bd70be3fb317ce7d255c8a4c3f4af30e298aa1aab8e3ba11ec2a2b39ab372c60c16b421536e50e5ce64a0bc81765c2e38381bcff6',
-      Path: 'https://registry.hub.docker.com/v2/library/alpine/blobs/sha256:8e3ba11ec2a2b39ab372c60c16b421536e50e5ce64a0bc81765c2e38381bcff6'
-    }]);
+
+  it('should actually successfully look up an actual docker image', async () => {
+    // generally skipped to avoid a network call
+    if (process.env.INTEGRATION) {
+      registry = new DockerRegistry('alpine');
+      const layers = await registry.getLayers();
+      expect(layers).to.eql([{
+        Name: '11cd0b38bc3ceb958ffb2f9bd70be3fb317ce7d255c8a4c3f4af30e298aa1aab8e3ba11ec2a2b39ab372c60c16b421536e50e5ce64a0bc81765c2e38381bcff6',
+        Path: 'https://registry.hub.docker.com/v2/library/alpine/blobs/sha256:8e3ba11ec2a2b39ab372c60c16b421536e50e5ce64a0bc81765c2e38381bcff6'
+      }]);
+    }
+  });
+
+  it('should actually successfully look up an actual docker image', async () => {
+    // generally skipped to avoid a network call
+    if (process.env.INTEGRATION) {
+      registry = new DockerRegistry('quay.io/coreos/clair');
+      const layers = await registry.getLayers(1);
+      expect(layers[0]).to.eql({
+        Name: 'ff3a5c916c92643ff77519ffa742d3ec61b7f591b6b7504599d95a4a41134e28',
+        Path: 'https://quay.io/v2/coreos/clair/blobs/sha256:ff3a5c916c92643ff77519ffa742d3ec61b7f591b6b7504599d95a4a41134e28'
+      });
+    }
   });
 
   afterEach(() => {
